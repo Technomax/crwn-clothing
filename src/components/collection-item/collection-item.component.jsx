b808ee18d991d5eca769254597b8920c6c61eaaa
Technomax <anil.maharjan@technomax.com.np>
2021-12-28 08:18:48 -0600

    Adding header

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -10,10 +10,10 @@ const CollectionItem = ({ id, name, price, imageUrl }) => (
         backgroundImage: `url(${imageUrl})`,
       }}
     />
-<div className="collection-footer">
-<span className="name">{name}</span>
-<span className="price">{price}</span>
-</div>
+    <div className="collection-footer">
+      <span className="name">{name}</span>
+      <span className="price">{price}</span>
+    </div>
   </div>
 );
 

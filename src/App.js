fed998344d4bbcd1cd3f4c6eaa3929eaada7629c
Technomax <anil.maharjan@technomax.com.np>
2021-12-29 23:09:05 -0600

    Integrated the sign in form using react component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 const HatsPage = () => (
   <div>
@@ -19,6 +20,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage/>} />
         <Route path="/shop" element={<ShopPage/>} />
+        <Route path="/signin" element={<SignInAndSignUpPage/>} />
       </Routes>
     </div>
   );
